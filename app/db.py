2c780fa471f2edfd252cb6e83bedcfe54a4160cc
warmlund <emeliewarmlund@gmail.com>
2026-02-18 20:19:32 +0100

    Added db.py for database creation

---
+++ app/db.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class User(SQLModel, table = True):
+    id: int | None = Field(default = None, primary_key = True)
+    name: str = Field(index = True)
+    email: str
+    password: str
+
+class Todo(SQLModel, table = True):
+    id: int | None = Field(default= None, primary_key= True)
+    title: str = Field(index = True)
+    description : str
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_table():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
