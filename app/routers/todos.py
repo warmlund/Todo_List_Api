13572aa5e768e26f7c637e206aa4cd2fb177fcbd
warmlund <emeliewarmlund@gmail.com>
2026-03-01 21:31:05 +0100

    Added functionality to add user id to added todo, added endpoint and functionality for updating todo

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -1,20 +1,26 @@
+from sqlmodel import select
 from fastapi import APIRouter, HTTPException, status, Depends
-from app.models.todo import Todo, TodoCreate
+from app.models.todo import Todo, TodoCreate, TodoUpdate
 from app.db import SessionDep
-from app.utils.todo import create_todo_in_db
+from app.utils.todo import create_todo_in_db, get_existing_todo, update_todo_in_db
 from app.utils.user import get_current_user
 
-router = APIRouter(prefix="/todos", tags=["todos"])
+router = APIRouter(tags=["todos"])
 
 @router.post("/todos", response_model=Todo)
 def create(todo: TodoCreate, session: SessionDep, current_user = Depends(get_current_user)):
-    new_todo = create_todo_in_db(todo, session)
+    new_todo = create_todo_in_db(todo, session, current_user.id)
     return new_todo
 
-#TODO edit endpoint for verifying user token and getting user from token - now todo items are not linked to any user, 
-# so we can create todo items without authentication, but in the future we will link todo items to users and then we will 
-# need to verify user token and get user from token before creating todo item
-#TODO: test endpoint with creating todo and verify that it returns correct information
+@router.put("/todos/{todo_id}", response_model=Todo)
+def update(todo_id: int, todo: TodoUpdate, session: SessionDep, current_user = Depends(get_current_user)):
+    updated_todo = update_todo_in_db(todo_id, todo, session, current_user.id)
+    return updated_todo
+
+@router.get("/gettodos")
+def read_todos(session: SessionDep, offset: int = 0, limit: int = 1000):
+    todos = session.exec(select(Todo).offset(offset).limit(limit)).all()
+    return todos
 # TODO: add enpoint for updating todo item
 #TODO: add endpoint for deleting todo item
 #TODO: add endpoint for getting all todo items for a user
